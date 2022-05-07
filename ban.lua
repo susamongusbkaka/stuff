4494b44b2f78d5bfafd8c65c271cb96ff750af62
susamongusbkaka <96744981+susamongusbkaka@users.noreply.github.com>
2022-05-07 13:52:25 +0100

    Update ban.lua

--- ban.lua
+++ ban.lua
@@ -1,1 +1,14 @@
-"KohlsH_Angler" or "whatjggotdeleted"
+if not _G.executed then
+while true do wait(5)
+local Banned = {
+  Players = {
+    "KohlsH_Angler",
+    "whatjggotdeleted"
+  }
+  }
+ 
+  for i,v in pairs(Banned.Players) do 
+    if game.Players.LocalPlayer.Name == v then game.Players.LocalPlayer:Kick("lol") end
+  end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/susamongusbkaka/stuff/main/ban.lua",true))()
+local _G.executed = true
